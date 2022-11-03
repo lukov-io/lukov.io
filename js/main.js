ff31d51af6330c067a387410315e7d689d6c1aa5
D1maSf <d1manzbandit@gmail.com>
2022-11-03 15:33:31 +0300

    must see, did a header

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
+/*hamb*/
 let hamb = document.querySelector(".hamb");
 let menuWrapper = document.querySelector(".wrapper-menu");
-let navi = document.querySelector(".navigation")
+
 hamb.addEventListener("click", function (event) {
     menuWrapper.classList.toggle("show-menu");
-    hamb.classList.toggle("hamb-right");
-
+    menuWrapper.classList.toggle("offcanvas");
+    hamb.classList.toggle("invert");
 })
 
 
-
+/*menu*/
 let dropMenuItems = document.querySelectorAll(".dropdown-menu__item");
 
 for (let drop of dropMenuItems) {
-    drop.addEventListener("click", function (e) {
-        e.stopPropagation();
-        e.currentTarget.classList.toggle("active");
-        console.log("result");
-        console.log(e, "event");
-
+    drop.addEventListener("click", function (event) {
+        event.stopPropagation();
+        let massItems = event.currentTarget.classList;
+        if (massItems.contains("dropdown-menu__item")){
+            let dropMenuItemsActive = document.querySelectorAll(".dropdown-menu__item.active");
+            dropMenuItemsActive.forEach(function (element) {
+                console.log(event.currentTarget, "event");
+                console.log(element, "element");
+                let currentTargetSubMenu = event.currentTarget.querySelectorAll(".dropdown-menu__content-inner");
+                console.log(currentTargetSubMenu, "currentTargetSubMenu");
+                if(element !== event.currentTarget && !currentTargetSubMenu) {
+                    element.classList.remove("active");
+                    console.log("[fe");
+                }
+            })
+            event.currentTarget.classList.toggle("active");
+        }
 })
 }
 
