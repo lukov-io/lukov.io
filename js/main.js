069a1875b8701fed0bb36786a129b9d7001a4337
D1maSf <d1manzbandit@gmail.com>
2022-11-08 13:07:04 +0300

    changes in header

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let hamb = document.querySelector(".hamb");
 let menuWrapper = document.querySelector(".wrapper-menu");
 
-hamb.addEventListener("click", function (event) {
+hamb.addEventListener("click", function () {
     menuWrapper.classList.toggle("show-menu");
     menuWrapper.classList.toggle("offcanvas");
     hamb.classList.toggle("invert");
@@ -19,13 +19,10 @@ for (let drop of dropMenuItems) {
         if (massItems.contains("dropdown-menu__item")){
             let dropMenuItemsActive = document.querySelectorAll(".dropdown-menu__item.active");
             dropMenuItemsActive.forEach(function (element) {
-                console.log(event.currentTarget, "event");
-                console.log(element, "element");
                 let currentTargetSubMenu = event.currentTarget.querySelectorAll(".dropdown-menu__content-inner");
                 console.log(currentTargetSubMenu, "currentTargetSubMenu");
                 if(element !== event.currentTarget && !currentTargetSubMenu) {
                     element.classList.remove("active");
-                    console.log("[fe");
                 }
             })
             event.currentTarget.classList.toggle("active");
