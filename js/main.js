39ae5087abf0474c9184975c54743b23a6f783ae
D1maSf <d1manzbandit@gmail.com>
2022-11-15 13:47:44 +0300

    fixes for header

--- js/main.js
+++ js/main.js
@@ -1,44 +1,29 @@
-// hamb
-let hamb = document.querySelector(".hamb");
-let menuWrapper = document.querySelector(".wrapper-menu");
+/* hamb */
+const hamb = document.querySelector('.hamb');
+const menuWrapper = document.querySelector('.wrapper-menu');
 
-hamb.addEventListener("click", function () {
-    menuWrapper.classList.toggle("show-menu");
-    menuWrapper.classList.toggle("offcanvas");
-    hamb.classList.toggle("invert");
+hamb.addEventListener('click', function() {
+  menuWrapper.classList.toggle('show-menu');
+  menuWrapper.classList.toggle('offcanvas');
+  hamb.classList.toggle('invert');
 })
 
+/* menu */
+const dropMenuItems = document.querySelectorAll('.dropdown-menu__item');
 
-// menu
-let dropMenuItems = document.querySelectorAll(".dropdown-menu__item");
-
-for (let drop of dropMenuItems) {
-    drop.addEventListener("click", function (event) {
-        event.stopPropagation();
-        let massItems = event.currentTarget.classList;
-        if (massItems.contains("dropdown-menu__item")){
-            let dropMenuItemsActive = document.querySelectorAll(".dropdown-menu__item.active");
-            dropMenuItemsActive.forEach(function (element) {
-                let currentTargetSubMenu = event.currentTarget.querySelectorAll(".dropdown-menu__content-inner");
-                console.log(currentTargetSubMenu, "currentTargetSubMenu");
-                if(element !== event.currentTarget && !currentTargetSubMenu) {
-                    element.classList.remove("active");
-                }
-            })
-            event.currentTarget.classList.toggle("active");
+for (const drop of dropMenuItems) {
+  drop.addEventListener('click', function(event) {
+    event.stopPropagation();
+    const massItems = event.currentTarget.classList;
+    if (massItems.contains('dropdown-menu__item')) {
+      const dropMenuItemsActive = document.querySelectorAll('.dropdown-menu__item.active');
+      dropMenuItemsActive.forEach(function(element) {
+        const currentTargetSubMenu = event.currentTarget.querySelectorAll('.dropdown-menu__content-inner');
+        if (element !== event.currentTarget && !currentTargetSubMenu) {
+          element.classList.remove('active');
         }
-})
+      })
+      event.currentTarget.classList.toggle('active');
+    }
+  })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
