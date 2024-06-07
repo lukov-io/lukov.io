5bed9d6ef5b9bcc15ff8b57a3db6dc6bdc6c187c
IktaNikuba <baukinv4@gmail.com>
2024-06-07 17:07:52 +0300

    Deleted .output from .gitignore for ssr

---
+++ .output/public/_nuxt/B7gZ0sDR.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./DYNYsLW_.js";const r={};function n(s,t){return o(),c("div",null,"Works")}const a=e(r,[["render",n]]);export{a as default};
---
+++ .output/public/_nuxt/BxPv5R5L.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as c}from"./DYNYsLW_.js";const n={};function t(o,s){return c(),r("div",null,"Partners")}const _=e(n,[["render",t]]);export{_ as default};
